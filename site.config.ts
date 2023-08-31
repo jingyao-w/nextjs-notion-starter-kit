eeee7f22d1d34544647c6336c59abaf6b9ea79ba
Jingyao Wang <143624785+jingyao-w@users.noreply.github.com>
2023-08-31 22:10:56 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,7 +9,7 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: ' Learn harder $ Earn more',
+  name: ' Study harder $ Earn more',
   domain: 'jingyao-w',
   author: 'J W',
 
@@ -31,7 +31,7 @@ export default siteConfig({
   defaultPageCoverPosition: 0.5,
 
   // whether or not to enable support for LQIP preview images (optional)
-  isPreviewImageSupportEnabled: false,
+  isPreviewImageSupportEnabled: true,
 
   // whether or not redis is enabled for caching generated preview images (optional)
   // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
